a8c881da89e28be50f9e45e183b72ecc5fdbb6cd
Neo <lordneobarnachea@gmail.com>
2023-05-26 21:26:21 +0800

    added assets and components

--- little-lemon/src/App.jsx
+++ little-lemon/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
-
+import Header from './components/Header'
 function App() {
 
 
   return (
     <>
-      <div className='main'>Hello world</div>
+      <Header/>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
     </>
   )
 }
