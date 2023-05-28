3bfe98ac292f54287e5d51b67f07bbaa6bc6ee0a
Neo <lordneobarnachea@gmail.com>
2023-05-28 11:29:46 +0800

    added the content of hero section

--- little-lemon/src/components/main/Main.jsx
+++ little-lemon/src/components/main/Main.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './main-styles/main.css'
+import image from './main-assets/restaurant chef B.jpg'
 export default function Main() {
   return (
     <>
       <div className='hero'>
+        <div className='heroBox'>
 
+          <div className='insideBox'>
+            <h1>Little Lemon</h1>
+            <p className='location'>Chicago</p>
+
+            <p className='description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, et similique.</p>
+
+            <button>Reserve a table</button>
+          </div>
+
+          <div className='imageBox'>
+            <img src={image} alt="restaurant chef" />
+          </div>
+
+
+        </div>
       </div>
+
+
+
+
+
+
       <div className='highlights'>
 
       </div>
