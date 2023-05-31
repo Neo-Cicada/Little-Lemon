dea9e1c65f370e7b6b0a228979b590d040fcb8cd
Neo <lordneobarnachea@gmail.com>
2023-05-31 20:15:22 +0800

    finished card component

--- little-lemon/src/components/main/highlights/Highlights.jsx
+++ little-lemon/src/components/main/highlights/Highlights.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import './highlights-styles/highlights.css'
 import Card from './highlights-components/Card'
+import Salad from '../main-assets/greek salad.jpg'
+import Brucheta from '../main-assets/bruchetta.svg'
+import Lemon from '../main-assets/lemon dessert.jpg'
 export default function Highlights() {
+  const item1 = {
+    description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic. ',
+     name: 'Greek Salad',
+    price: '$12.99'
+  }
+  const item2 = {
+    description: 'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. ',
+    name: 'Bruchetta',
+    price: '$ 5.99'
+  }
+  const item3 = {
+    description: 'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic.',
+    name: 'Lemon Dessert',
+    price: '$ 5.00'
+  }
   return (
     <>
       <div className='highContainer'>
@@ -10,9 +28,31 @@ export default function Highlights() {
           <button>Online Menu</button>
         </div>
         <div className='bodyLights'>
-          <Card description='lorem epsum' />
-          <Card />
-          <Card />
+          <div className='item'>
+            <Card
+              description={item1.description}
+              image={Salad}
+              name={item1.name}
+              price={item1.price}
+            />
+          </div>
+          <div className='item'>
+            <Card
+              image={Salad}
+              name={item2.name}
+              price={item2.price}
+              description={item2.description}
+            />
+          </div>
+          <div className="item">
+            <Card
+              image={Lemon}
+              name={item3.name}
+              price={item3.price}
+              description={item3.description}
+            />
+          </div>
+
         </div>
       </div>
     </>
--- little-lemon/src/components/main/highlights/highlights-components/Card.jsx
+++ little-lemon/src/components/main/highlights/highlights-components/Card.jsx
@@ -1,24 +1,69 @@
 import React from 'react'
 
 const styleCard= {
-    heigh: '439px',
-    width: '264px',
-    border: '1px solid red',
+    borderRadius: '16px 16px 0px 0px',
     backgroundColor: '#EDEFEE'
 }
 
+const img = {
+  width: '100%',
+  height: '100%',
+  borderRadius: '16px 16px 0px 0px',
+
+}
+const nameAndPrice = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginLeft: '10%',
+  marginRight: '10%',
+  marginTop: '5%'
+}
+const name = {
+
+  fontFamily: 'Markazi Text',
+  fontStyle: 'normal',
+  fontWeight: '500',
+  fontSize: '1.2rem',
+  lineHeight: '90%',
+  color: '#000000'
+}
+
+const price = {
+
+  fontFamily: 'Karla',
+  fontStyle: 'normal',
+  fontWeight: '700',
+  fontSize: '1.2rem',
+  lineHeight: '19px',
+
+  color: '#EE9972',
+}
+const description = {
+  marginLeft: '10%',
+  marginRight: '10%',
+  marginTop: '5%',
+  height: '30%',
+
+
+
+}
 export default function Card(props) {
   return (
     <>
         <div style={styleCard}>
-            <div className='img'>
-              <img src={props.image} alt='product name' />
-            </div>
-            <div className="nameAndPrice">
+          <div className="imgContainer" style={{height: '12em'}}>
+            <img src={props.image} alt='product name' style={img} />
+          </div>
 
+            <div className="nameAndPrice" style={nameAndPrice}>
+              <div style={name}>{props.name}</div>
+              <div style={price}>{props.price}</div>
             </div>
             <div className="foodDescription">
-              <p>{props.description}</p>
+              <p style={description}>{props.description}</p>
+            </div>
+            <div style={{marginLeft:'10%', marginTop: '5%' }}>
+              <p style={{color: '#333333', fontWeight: '700'}}> Order a delivery</p>
             </div>
         </div>
     </>
