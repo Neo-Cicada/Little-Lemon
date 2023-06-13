adc7d005d3130507d8e90c70e3c388a8b1188619
Neo <lordneobarnachea@gmail.com>
2023-06-13 11:13:10 +0800

    added a function that collects the data of the form

--- little-lemon/src/components/reservation/Reservation.jsx
+++ little-lemon/src/components/reservation/Reservation.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState } from 'react';
 import './reservation-style/reservation.css'
 export default function Reservation() {
+    const [formData, setFormData] = useState({});
+    const handleInput = (event) =>{
+        let newData = {[event.target.name]: event.target.value};
+        setFormData({...formData, ...newData})
+    }
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(formData)
+
+    }
     return (
         <>
             <div className='reservation-container'>
@@ -12,42 +23,83 @@ export default function Reservation() {
                         <h2>Reservation</h2>
                     </div>
 
-                    <form action="#">
+                    <form action="#" onSubmit={handleSubmit}>
                         <div className='first-field'>
                             <div>
-                                <input type="text" name="firstname" id="firstname" placeholder='First Name' />
+                                <input
+                                    type="text"
+                                    name="firstname"
+                                    id="firstname"
+                                    placeholder='First Name'
+                                    onChange={(event) => handleInput(event)}
+                                    required />
                             </div>
                             <div>
-                                <input type="text" name='lastname' id='lastname' placeholder='Last Name' />
+                                <input
+                                type="text"
+                                name='lastname'
+                                id='lastname'
+                                placeholder='Last Name'
+                                onChange={(event) => handleInput(event)}
+
+                                required />
                             </div>
                         </div>
                         <div className='second-field'>
                             <div>
-                                <input type="datetime-local" name="time and date" id='tnd' />
+                                <input
+                                type="date"
+                                name="date"
+                                id='tnd'
+                                onChange={(event) => handleInput(event)}
+
+                                required />
                             </div>
                             <div>
-                                <input type="number" name="quantity" id="quantity" placeholder='Number of people' />
+                                <select
+                                 name='res-time'
+                                 id="res-time"
+                                 onChange={(event) => handleInput(event)}
+                                 placeholder='10:00'
+                                 >
+                                    <option value='10:00'>10:00</option>
+                                    <option value='11:00'>11:00</option>
+                                    <option value='12:00'>12:00</option>
+                                    <option value='13:00'>13:00</option>
+                                    <option value='17:00'>17:00</option>
+                                    <option value='18:00'>18:00</option>
+                                    <option value='19:00'>19:00</option>
+                                    <option value='20:00'>20:00</option>
+                                    <option value='21:00'>21:00</option>
+                                    <option value='22:00'>22:00</option>
+                                </select>
                             </div>
 
                         </div>
-
                         <div className='third-field'>
                             <div>
-                                <textarea type="text" name="request" id="request" placeholder='Special request' />
+                                <input
+                                 type="number"
+                                 name="quantity"
+                                 id="quantity"
+                                 placeholder='Number of guests'
+                                 min='1'
+                                 max='10'
+                                 onChange={(event) => handleInput(event)}
+
+                                 required />
                             </div>
                             <div>
-                                <div>
-                                    <label htmlFor="email">Email</label>
-                                    <input type="checkbox" name="email" id="email" />
-                                </div>
-                                <div>
-                                    <label htmlFor="sms">SMS</label>
-                                    <input type="checkbox" name="sms" id="sms" />
-
-                                </div>
+                                <textarea
+                                type="text"
+                                name="request"
+                                id="request"
+                                placeholder='Special request'
+                                onChange={(event) => handleInput(event)}
+                                />
                             </div>
                             <div className='btn-area'>
-                                <button>Book now</button>
+                                <button type='submit'>Book now</button>
                             </div>
                         </div>
                     </form>
